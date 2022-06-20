3d58620bdf9e3a0e532fc7264336da84fdaddde3
lwl <435688792@qq.com>
2022-06-20 18:33:59 +0800

    新增了一个abc类

---
+++ meinian_common/src/main/java/com/atguigu/util/ABC.java
@@ -0,0 +1,14 @@
+package com.atguigu.util;
+
+/**
+ * @program: meinian_parent
+ * @description:
+ * @author: lwl
+ * @create: 2022-06-20 18:32
+ */
+public class ABC {
+    public static void main(String[] args) {
+        System.out.println("111111111111111111");
+
+    }
+}
